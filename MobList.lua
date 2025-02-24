9d5b724c7bb067a60f19e9216c3ebcac7b8fa455
FDHUB <92850171+FluxDevSs@users.noreply.github.com>
2025-02-24 22:13:37 +1100

    Update MobList.lua

--- MobList.lua
+++ MobList.lua
@@ -1,3 +1,23 @@
 MobList = {
-""
+  Earth = {
+    "Alien",
+    "Android 17"
+    "Android 18",
+    "Android Prototype A",
+    "Android Prototype B",
+    "Android Prototype S",
+    "Bear",
+    "Big Snake",
+    "Black Scorpian",
+    "Brute",
+    "Chi Expert",
+    "Corrupt Officer",
+    "Evil Crane Student",
+    "Evil Majin",
+    "Evil Namek",
+    "Evil Saiyan",
+    "Evil Saiyan [SSJ]",
+    "Galactic Patrol",
+    "Gorila",
+  }
 }
