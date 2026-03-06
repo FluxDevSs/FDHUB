b6e5e13861a7fced3fa5c7bedef970768a4c5cfd
FDHUB <92850171+FluxDevSs@users.noreply.github.com>
2026-03-07 10:37:51 +1100

    Update ESPLib.lua

--- ESPLib.lua
+++ ESPLib.lua
@@ -23,7 +23,7 @@ ESP.Settings = {
     BoxThickness = 1.5,
 
     -- Name
-    NameEnabled = false
+    NameEnabled = false,
 
     -- Skeleton
     SkeletonEnabled = false,
@@ -445,3 +445,4 @@ RunService.RenderStepped:Connect(function()
 end)
 
 return ESP
+
